6f14e31c8fe9d51ed57b7fb93b7030e6d44b8420
Mistigrex <sacha.garnier@live.fr>
2019-05-03 14:44:01 +0200

    Modèle 2.0.4

---
+++ model/IDogfightModel.java
@@ -0,0 +1,9 @@
+package model; //déclaration du package dans lequel se trouve la classe
+
+/**
+ * IDogfightModel
+ */
+public interface IDogfightModel {   //création de l'interface IDogfightModel
+
+    
+}
---
+++ model/IMobile.java
@@ -0,0 +1,6 @@
+package model; //déclaration du package dans lequel se trouve la classe
+
+public interface IMobile {  //création de l'interface IMobile
+
+    
+}
